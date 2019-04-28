0b41fb4d3af2816bcb57e0b392a84c60b8b66ed8
Addison <addison.bohannon@gmail.com>
2019-04-28 16:05:37 -0400

    added lasso coef update and validated

--- solver.py
+++ solver.py
@@ -9,7 +9,7 @@ Date: 28 Apr 2019
 import numpy as np
 import scipy.linalg as sl
 import scipy.spatial.distance as sd
-from sklearn.linear_model import orthogonal_mp_gram
+from sklearn.linear_model import orthogonal_mp_gram, lars_path
 
 def ip(a, b):
     """
@@ -42,7 +42,7 @@ def coeff_update_omp_sklearn(Y, X, D, s):
     
     outputs:
     coefficients (c) - r tensor
-    """    
+    """
     # precompute reused quantities
     r, _, _ = D.shape
     XTX = np.dot(X.T, X)
@@ -98,3 +98,19 @@ def coeff_update_omp(Y, X, D, s):
         # update residual
         R = Y - np.tensordot(c[J_index], np.matmul(X, D[J_index, :, :]), axes=1)
     return c
+
+def coeff_update_lasso_sklearn(Y, X, D, s):
+    # precompute reused quantities
+    r, _, _ = D.shape
+    XTX = np.dot(X.T, X)
+    XTY = np.dot(X.T, Y)
+    XD_gram = []
+    indices = np.triu_indices(r, k=1)
+    [XD_gram.append(ip(D[i, :, :], np.dot(XTX, D[j, :, :]))) for (i, j) in zip(indices[0], indices[1])]
+    XD_gram = np.array(XD_gram)
+    XD2 = np.sum(np.square(np.matmul(X, D)), axis=(1,2))
+    DTXTXD = sd.squareform(XD_gram) + np.diagflat(XD2)
+    YTXD = np.array([ip(XTY, D[j, :, :]) for j in range(r)])
+    # call sklearn subroutine
+    _, _, coefs = lars_path(X, Y, Xy=YTXD, Gram=DTXTXD, method='lasso')
+    return coefs[:, s]
