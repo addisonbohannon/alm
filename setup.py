d7b933291bfa8b3075b97d8581df84424b642294
Addison <addison.bohannon@gmail.com>
2019-10-18 15:28:14 -0400

    refactoring file structure

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from setuptools import setup
 
 setup(
-    name='almm',
+    name='alm',
     version='0.0.0',
-    packages=['almm', 'application', 'validation'],
+    packages=['alm', 'experiments'],
     url='https://gitlab.sitcore.net/addison.bohannon/almm',
     license='',
     author='Addison Bohannon',
