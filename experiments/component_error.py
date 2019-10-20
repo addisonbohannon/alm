c8469bd03762319efdf1d1a22c013047170dbc89
addison.bohannon <addison.bohannon@gmail.com>
2019-10-20 06:48:30 -0400

    adding analyses

---
+++ experiments/component_error.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import numpy as np
+import matplotlib.pyplot as plt
+from almm.utility import initialize_components
+from validation.utility import component_distance
+
+samples = 10000
+num_components = 10
+model_order = 12
+signal_dimension = 6
+
+D = [(initialize_components(num_components, model_order, signal_dimension), 
+      initialize_components(num_components, model_order, signal_dimension))
+     for _ in range(samples)]
+cdist = [component_distance(D_i, D_j)[0] for (D_i, D_j) in D]
+plt.hist(cdist)
+p = np.percentile(cdist, 5)
--- experiments/n_vs_m.py
+++ experiments/n_vs_m.py
@@ -8,15 +8,13 @@ from itertools import product
 from multiprocessing import Pool
 import pickle
 import numpy.random as nr
-from alm.alm import Almm
-from alm.utility import unstack_coef, initialize_components
-from experiments.sampler import alm_sample
-from experiments.utility import component_distance
+from almm.almm import Almm
+from almm.utility import unstack_coef, initialize_components
+from validation.sampler import almm_sample
+from validation.utility import component_distance
 
-n = 1000
-dn = 200
-m = 10000
-dm = 2000
+n = [10, 100, 1000]
+m = [10, 100, 1000, 10000]
 d = 5
 r = 10
 p = 2
@@ -24,25 +22,25 @@ s = 3
 N = 10
 k = 10
 mu = 1e-2
-NUM_PROCESSES = 5
+NUM_PROCESSES = 3
 
 
 def n_vs_m(experiment_id):
     # Set seed
     nr.seed()
-    # Generate alm samples
-    x, _, D = alm_sample(n, m, d, r, p, s, coef_condition=1e2, component_condition=1e2)
+    # Generate almm samples
+    x, _, D = almm_sample(max(n), max(m), d, r, p, s, coef_condition=1e2, component_condition=1e2)
     # Initialize variables
     palm_component_error, palm_likelihood = [], []
     altmin_component_error, altmin_likelihood = [], []
     bcd_component_error, bcd_likelihood = [], []
+    # Generate initial autoregressive component estimate
+    D_0 = [initialize_components(r, p, d) for _ in range(k)]
     # Implement solver for varying number of observations and length of observations
-    for (n_i, m_i) in product(range(0, n, dn), range(0, m, dm)):
-        # Generate initial autoregressive component estimate
-        D_0 = [initialize_components(r, p, d) for _ in range(k)]
+    for (n_i, m_i) in product(n, m):
         # PALM
-        alm_model = Almm(solver='palm')
-        Di_palm, _, Li_palm, _ = alm_model.fit(x[:(n_i+dn-1), :(m_i+dm-1), :], p, r, mu, num_starts=k,
+        almm_model = Almm(solver='palm')
+        Di_palm, _, Li_palm, _ = almm_model.fit(x[:(n_i-1), :(m_i-1), :], p, r, mu, num_starts=k,
                                                 initial_component=D_0, return_all=True)
         palm_likelihood.append(Li_palm)
         loss_palm = []
@@ -52,8 +50,8 @@ def n_vs_m(experiment_id):
             loss_palm.append(d_loss)
         palm_component_error.append(loss_palm)
         # AltMin
-        alm_model = Almm(solver='altmin')
-        Di_altmin, _, Li_altmin, _ = alm_model.fit(x[:(n_i+dn-1), :(m_i+dm-1), :], p, r, mu, num_starts=k,
+        almm_model = Almm(solver='altmin')
+        Di_altmin, _, Li_altmin, _ = almm_model.fit(x[:(n_i-1), :(m_i-1), :], p, r, mu, num_starts=k,
                                                     initial_component=D_0, return_all=True)
         altmin_likelihood.append(Li_altmin)
         loss_altmin = []
@@ -63,8 +61,8 @@ def n_vs_m(experiment_id):
             loss_altmin.append(d_loss)
         altmin_component_error.append(loss_altmin)
         # BCD
-        alm_model = Almm(solver='bcd')
-        Di_bcd, _, Li_bcd, _ = alm_model.fit(x[:(n_i+dn-1), :(m_i+dm-1), :], p, r, mu, num_starts=k,
+        almm_model = Almm(solver='bcd')
+        Di_bcd, _, Li_bcd, _ = almm_model.fit(x[:(n_i-1), :(m_i-1), :], p, r, mu, num_starts=k,
                                               initial_component=D_0, return_all=True)
         bcd_likelihood.append(Li_bcd)
         loss_bcd = []
@@ -74,8 +72,8 @@ def n_vs_m(experiment_id):
             loss_bcd.append(d_loss)
         bcd_component_error.append(loss_bcd)
     # Save results
-    path = "/home/addison/Python/alm/results"
-    with open(join(path, "n_vs_m-" + dt.now().strftime("%y%b%d_%H%M") + '-' + str(getpid()) + ".pickle"), 'wb') as f:
+    path = "/home/addison/Python/almm/results"
+    with open(join(path, "n_vs_m-log-" + dt.now().strftime("%y%b%d_%H%M") + '-' + str(getpid()) + ".pickle"), 'wb') as f:
         pickle.dump([palm_component_error, altmin_component_error, bcd_component_error, palm_likelihood,
                      altmin_likelihood, bcd_likelihood], f)
 
