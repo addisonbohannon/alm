e8ca8e62d5a1c24c9beeeb234404d8d93289a672
Addison <addison.bohannon@gmail.com>
2019-10-24 06:03:43 -0400

    updated for collecting multiple samples

---
+++ experiments/model_order_misspecification.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from os.path import join
+import pickle
+import numpy as np
+import matplotlib.pyplot as plt
+from alm.alm import Alm
+from experiments.sampler import alm_sample
+
+NUM_OBS = 1000
+OBS_LEN = 10000
+SIGNAL_DIM = 5
+NUM_COMPONENTS = 10
+MODEL_ORDER = [1, 2, 4, 8, 16]
+COEF_SUPP = 3
+NUM_STARTS = 1
+PENALTY_PARAM = 1e-2
+SAVE_PATH = '/home/addison/Python/almm/results'
+NUM_SAMPLES = 10
+
+nll = np.zeros([NUM_SAMPLES, len(MODEL_ORDER), len(MODEL_ORDER)])
+for sample in range(NUM_SAMPLES):
+    for j, model_order_gen in enumerate(MODEL_ORDER):
+        data, _, D = alm_sample(NUM_OBS, OBS_LEN, SIGNAL_DIM, NUM_COMPONENTS, model_order_gen, COEF_SUPP,
+                                coef_condition=1e2, component_condition=1e2)
+        for i, model_order_fit in enumerate(MODEL_ORDER):
+            print('Generative model order: ' + str(model_order_gen) + ', Fitted model order: ' + str(model_order_fit))
+            alm_model = Alm(solver='palm', tol=1e-3)
+            _, _, nll[sample, i, j], _ = alm_model.fit(data, model_order_fit, NUM_COMPONENTS, PENALTY_PARAM, num_starts=NUM_STARTS)
+    fig, ax = plt.subplots()
+    ax.set_xlabel('Generative model order')
+    ax.set_xticks([0, 1, 2, 3])
+    ax.set_xticklabels(MODEL_ORDER)
+    ax.set_ylabel('Fitted model order')
+    ax.set_yticks([0, 1, 2, 3])
+    ax.set_yticklabels(MODEL_ORDER)
+    image = ax.imshow(np.mean(nll, axis=0), origin='lower', cmap=plt.cm.Blues)
+    fig.colorbar(image, fraction=0.046, pad=0.04)
+    plt.savefig(join(SAVE_PATH, 'model_order_misspecification.eps'))
+    with open(join(SAVE_PATH, 'model_order_misspecification.pickle'), 'wb') as f:
+        pickle.dump(nll, f)
---
+++ experiments/num_components_misspecification.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from os.path import join
+import pickle
+import numpy as np
+import matplotlib.pyplot as plt
+from alm.alm import Alm
+from experiments.sampler import alm_sample
+
+NUM_OBS = 1000
+OBS_LEN = 10000
+SIGNAL_DIM = 5
+NUM_COMPONENTS = [1, 2, 4, 8, 16]
+MODEL_ORDER = 2
+COEF_SUPP = 3
+NUM_STARTS = 1
+PENALTY_PARAM = 1e-2
+SAVE_PATH = '/home/addison/Python/almm/results'
+NUM_SAMPLES = 10
+
+nll = np.zeros([NUM_SAMPLES, len(NUM_COMPONENTS), len(NUM_COMPONENTS)])
+for sample in range(NUM_SAMPLES):
+    for j, num_comps_gen in enumerate(NUM_COMPONENTS):
+        data, _, D = alm_sample(NUM_OBS, OBS_LEN, SIGNAL_DIM, num_comps_gen, MODEL_ORDER, COEF_SUPP, coef_condition=1e2,
+                                component_condition=1e2)
+        for i, num_comps_fit in enumerate(NUM_COMPONENTS):
+            print('Generative number of components: ' + str(num_comps_gen) + ', Fitted number of components: '
+                  + str(num_comps_fit))
+            alm_model = Alm(solver='palm', tol=1e-3)
+            _, _, nll[sample, i, j], _ = alm_model.fit(data, MODEL_ORDER, num_comps_fit, PENALTY_PARAM, num_starts=NUM_STARTS)
+    fig, ax = plt.subplots()
+    ax.set_xlabel('Generative number of components')
+    ax.set_xticks([0, 1, 2, 3])
+    ax.set_xticklabels(NUM_COMPONENTS)
+    ax.set_ylabel('Fitted number of components')
+    ax.set_yticks([0, 1, 2, 3])
+    ax.set_yticklabels(NUM_COMPONENTS)
+    image = ax.imshow(np.mean(nll, axis=0), origin='lower', cmap=plt.cm.Blues)
+    fig.colorbar(image, fraction=0.046, pad=0.04)
+    plt.savefig(join(SAVE_PATH, 'num_components_misspecification.eps'))
+    with open(join(SAVE_PATH, 'num_components_misspecification.pickle'), 'wb') as f:
+        pickle.dump(nll, f)
