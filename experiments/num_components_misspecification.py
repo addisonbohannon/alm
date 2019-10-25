7c92ee17249104f258c19c7bfd60efbbf8717e15
addison.bohannon <addison.bohannon@gmail.com>
2019-10-25 14:07:22 -0400

    updating experiments

--- experiments/num_components_misspecification.py
+++ experiments/num_components_misspecification.py
@@ -13,7 +13,7 @@ OBS_LEN = 10000
 SIGNAL_DIM = 5
 NUM_COMPONENTS = [1, 2, 4, 8, 16]
 MODEL_ORDER = 2
-COEF_SUPP = 3
+COEF_SUPP = [1, 1, 1, 3, 3]
 NUM_STARTS = 1
 PENALTY_PARAM = 1e-2
 SAVE_PATH = '/home/addison/Python/almm/results'
@@ -21,8 +21,8 @@ NUM_SAMPLES = 10
 
 nll = np.zeros([NUM_SAMPLES, len(NUM_COMPONENTS), len(NUM_COMPONENTS)])
 for sample in range(NUM_SAMPLES):
-    for j, num_comps_gen in enumerate(NUM_COMPONENTS):
-        data, _, D = alm_sample(NUM_OBS, OBS_LEN, SIGNAL_DIM, num_comps_gen, MODEL_ORDER, COEF_SUPP, coef_condition=1e2,
+    for j, (num_comps_gen, coef_supp) in enumerate(zip(NUM_COMPONENTS, COEF_SUPP)):
+        data, _, D = alm_sample(NUM_OBS, OBS_LEN, SIGNAL_DIM, num_comps_gen, MODEL_ORDER, coef_supp, coef_condition=1e2,
                                 component_condition=1e2)
         for i, num_comps_fit in enumerate(NUM_COMPONENTS):
             print('Generative number of components: ' + str(num_comps_gen) + ', Fitted number of components: '
@@ -31,10 +31,10 @@ for sample in range(NUM_SAMPLES):
             _, _, nll[sample, i, j], _ = alm_model.fit(data, MODEL_ORDER, num_comps_fit, PENALTY_PARAM, num_starts=NUM_STARTS)
     fig, ax = plt.subplots()
     ax.set_xlabel('Generative number of components')
-    ax.set_xticks([0, 1, 2, 3])
+    ax.set_xticks([0, 1, 2, 3, 4])
     ax.set_xticklabels(NUM_COMPONENTS)
     ax.set_ylabel('Fitted number of components')
-    ax.set_yticks([0, 1, 2, 3])
+    ax.set_yticks([0, 1, 2, 3, 4])
     ax.set_yticklabels(NUM_COMPONENTS)
     image = ax.imshow(np.mean(nll, axis=0), origin='lower', cmap=plt.cm.Blues)
     fig.colorbar(image, fraction=0.046, pad=0.04)
--- experiments/periodogram.py
+++ experiments/periodogram.py
@@ -9,7 +9,7 @@ from experiments.utility import load_individual_results, periodogram_from_filter
 SAMPLING_RATE = 200
 FFT_LEN = 100
 
-components, _, _ = load_individual_results(8, start=0)
+components, _, _ = load_individual_results(11, start=0)
 periodogram = [periodogram_from_filter(unstack_coef(component_j), SAMPLING_RATE, fft_len=FFT_LEN)
                for component_j in components]
 fig, axs = plt.subplots(len(components), 1, sharey=True)
