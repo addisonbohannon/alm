ab95a5a441be6432cd1249041bcbaaa0655f30d4
Addison <addison.bohannon@gmail.com>
2019-11-15 09:38:19 -0500

    add correct ic metrics

--- experiments/model_selection.py
+++ experiments/model_selection.py
@@ -16,12 +16,16 @@ data, _ = load_isruc_data(SUBJ)
 num_obs, obs_len, sig_dim = data.shape
 nll = np.zeros([len(MODEL_ORDER), len(NUM_COMPS)])
 num_params = np.zeros_like(nll)
+num_params_wcoef = np.zeros_like(nll)
 for i, model_ord in enumerate(MODEL_ORDER):
     for j, num_comps in enumerate(NUM_COMPS):
-        num_params = model_ord * num_comps
+        num_params[i, j] = model_ord * num_comps * sig_dim**2
+        num_params_wcoef[i, j] = num_params[i, j] + num_obs * num_comps 
         alm_model = Alm(tol=1e-3, solver='palm', verbose=False)
         _, _, nll[i, j], _ = alm_model.fit(data, model_ord, num_comps, PENALTY_PARAM, num_starts=NUM_STARTS)
 """ aic: -2 * log(L) + 2 * k """
 aic = 2 * (num_obs * obs_len * nll + num_params)
-""" bic: -2 * log(L) + sqrt(n) * k """
+aic_wcoef = 2 * (num_obs * obs_len * nll + num_params_wcoef)
+""" bic: -2 * log(L) + log(n) * k """
 bic = 2 * num_obs * obs_len * nll + (np.log(num_obs * obs_len)) * num_params
+bic_wcoef = 2 * num_obs * obs_len * nll + (np.log(num_obs * obs_len)) * num_params_wcoef
