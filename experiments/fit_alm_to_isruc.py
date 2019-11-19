b08fa534fef51fa89d0ce7ae6c713aeafa7c4e68
addison.bohannon <addison.bohannon@gmail.com>
2019-11-19 17:23:41 -0500

    updating model_ord and num_comps

--- experiments/fit_alm_to_isruc.py
+++ experiments/fit_alm_to_isruc.py
@@ -6,14 +6,15 @@ import pickle
 from alm.alm import Alm
 from experiments.utility import load_isruc_data
 
-MODEL_ORD = 6
-NUM_COMPS = 8
+MODEL_ORD = 4
+NUM_COMPS = 10
 SUBJECTS = range(1, 11)
 PENALTY_PARAM = 1e-1
 NUM_STARTS = 5
 RESULTS_PATH = '/home/addison/Python/almm/results/individual'
 
 for subj in SUBJECTS:
+    print(subj)
     data, _ = load_isruc_data(subj)
     alm_model = Alm(tol=1e-3, solver='palm')
     ar_comps, mixing_coef, nll, _ = alm_model.fit(data, MODEL_ORD, NUM_COMPS, PENALTY_PARAM, num_starts=NUM_STARTS,
