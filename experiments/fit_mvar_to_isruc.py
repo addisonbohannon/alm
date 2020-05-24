a4999ac7a00aafbdc204b92fa48662cad2691d23
Addison <addison.bohannon@gmail.com>
2020-05-24 07:33:22 -0400

    saving nll

--- experiments/fit_mvar_to_isruc.py
+++ experiments/fit_mvar_to_isruc.py
@@ -11,7 +11,7 @@ from alm.utility import circulant_matrix, initialize_autoregressive_components
 from experiments.utility import load_isruc_data, save_results
 
 MAX_ITER = int(1e2)
-TOL = 1e-4
+TOL = 1e-3
 NUM_STARTS = 5
 SUBJS = np.arange(1, 11)
 MODEL_ORD = 12
@@ -37,7 +37,6 @@ def expectation_maximization(X, Y, num_comps, max_iter, tol):
         for j in range(num_comps):
             tau[:, :, j] = np.expand_dims(mixing_coef[:, j], axis=1) * np.exp(-0.5 * sl.norm(Y-np.matmul(X, ar_coef[j]), axis=2))
         nll = - np.sum(np.log(np.sum(tau, axis=2)))
-        print(nll)
         tau = tau / np.sum(tau, axis=2, keepdims=True)
         
         # Maximization
@@ -94,4 +93,4 @@ for subj in SUBJS:
     print('Subject: ' + str(subj))
     data, labels = load_isruc_data(subj)
     ar_comps, mixing_coef, nll = fit(data, MODEL_ORD, NUM_COMPS, NUM_STARTS, MAX_ITER, TOL)
-    save_results([ar_comps, mixing_coef, labels], 'S' + str(subj) + '-mvar-r6p12.pickle')
+    save_results([ar_comps, mixing_coef, labels, nll], 'S' + str(subj) + '-mvar-r6p12.pickle')
